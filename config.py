207e0db5ef52327b3612a0a8c7e5cf4d5c019ff5
Bhavya Patwa <bhavyapatwa007@gmail.com>
2020-02-01 02:45:38 +0000

    data loader read and plot stations on hdf5 + ncdf files

--- config.py
+++ config.py
@@ -1,17 +1,30 @@
-import argparse,os
+import argparse
+import json
+import os
 
 ROOT_DATA_PATH = '/project/cq-training-1/project1/data'
 RAW_DATA_CATALOG = 'catalog.helios.public.20100101-20160101.pkl'
 TEST_DATA_CATALOG = 'dummy_test_catalog.pkl'
 
-# SURFRAD Station codes
-STATION_CODES = ['BND','TBL','DRA','FPK','GWN','PSU','SXF']
+# SURFRAD Station Info
+# Format: { Station Code : [Latitude,Longitude,Elevation] }
+
+CHANNELS = ["ch1","ch2","ch3","ch4","ch6"]
+STATION_INFO = json.loads(open('SURFRAD.json').read())
 
 def init_args():
 	parser = argparse.ArgumentParser(description='Process arguments for processing/training/testing')
 	parser.add_argument('--mode', help='train/test/prepro')
-	parser.add_argument('--image_data', choices=['hdf5v7_8bit','hdf5v5_16bit'], help='choose either hdf5 8bit or 16bit')
-	parser.add_argument('--data_catalog',help='catalog file (dataframe) containing image path, ghi and other metadata')
-	
+	parser.add_argument('--image_data', choices=['hdf5v7_8bit','hdf5v5_16bit','netcdf'], default='hdf5v5_16bit',
+		help='choose either hdf5 8bit or 16bit')
+
+	data_catalog = os.path.join(ROOT_DATA_PATH,RAW_DATA_CATALOG)
+	parser.add_argument('--data_catalog_path',default=data_catalog,help='catalog absolute file path (dataframe) which contains the image paths, ghi and other metadata')
+	parser.add_argument('--station_data',default=STATION_INFO,help='catalog absolute file path (dataframe) which contains the image paths, ghi and other metadata')
+	parser.add_argument('--channels',default=CHANNELS,help='catalog absolute file path (dataframe) which contains the image paths, ghi and other metadata')
+
 	args = parser.parse_args()
+
+	# args["station_data"] = STATION_INFO
+	# args["channels"] = CHANNELS
 	return args
