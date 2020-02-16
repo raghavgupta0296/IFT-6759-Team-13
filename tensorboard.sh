2658ae9be8caccef91f9887e01144f6b9da7e49a
Bhavya Patwa <bhavyapatwa007@gmail.com>
2020-02-16 18:24:16 +0000

    Deleted Raghav's notebook files and irrelevant/stale scripts

--- tensorboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=60:00
-#SBATCH --gres=gpu:k80:1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10000M
-
-source ift6759-env/bin/activate
-
-tensorboard --logdir logs --bind_all
