f8c7f05bd2c5a001a3c16159488f3fbedd96b0b7
EmYassir <yassir.elmesbahi@outlook.com>
2020-02-20 23:33:00 -0500

    utility

--- utilities/utility.py
+++ utilities/utility.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tqdm import tqdm
+import os,binascii
 
 import config
 from utilities import utils
@@ -25,7 +26,7 @@ Args:
 Returns:
     string of size 'length' containing random characters
 """
-def get_file_name(length = 15):
+def generate_file_name(length = 10):
     return binascii.b2a_hex(os.urandom(length)).decode('ascii')
 
 # loads the pickle dataframe containing data paths and targets information
