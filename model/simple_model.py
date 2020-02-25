30ba0d4d108bc8e6eaad0237d14825af662cc6e0
Bhavya Patwa <bhavyapatwa007@gmail.com>
2020-02-25 18:19:03 +0000

    simple model+dataloader

---
+++ model/simple_model.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Flatten, Input, Dense, MaxPool2D, BatchNormalization, ReLU
+from tensorflow.keras import Model
+from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.optimizers import Adam
+from utilities import config
+from utilities.dataloader2 import SequenceDataLoaderMemChunks
+from utilities.dataloader_simple import SimpleDataLoader
+from utilities.utility import load_catalog
+import pdb
+
+class MyModel(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.input_0 = Input(shape=(70,70,5))
+        self.conv1 = Conv2D(32, 3, kernel_initializer='he_uniform')
+        self.batchnorm1 = BatchNormalization()
+        self.relu = ReLU()
+        self.maxpool = MaxPool2D((2,2))
+        self.conv2 = Conv2D(64, 3, kernel_initializer='he_uniform')
+        self.batchnorm2 = BatchNormalization()
+        self.conv3 = Conv2D(128, 3, kernel_initializer='he_uniform')
+        self.batchnorm3 = BatchNormalization()
+        self.conv4 = Conv2D(256, 3, kernel_initializer='he_uniform')
+        self.batchnorm4 = BatchNormalization()
+        self.conv5 = Conv2D(512, 5, kernel_initializer='he_uniform')
+        self.batchnorm5 = BatchNormalization()
+        self.flatten = Flatten()
+        self.d1 = Dense(1, activation='linear')
+
+    def call(self, x):
+        # x = x[0]
+        x = self.input_0(x)
+        x = self.conv1(x) # 68 x 68 x 32
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+        x = self.maxpool(x) # 34 x 34 x 32
+        x = self.conv2(x) # 32 x 32 x 64
+        x = self.batchnorm2(x)
+        x = self.relu(x)
+        x = self.maxpool(x) # 16 x 16 x 64
+        x = self.conv3(x) # 14 x 14 x 128
+        x = self.batchnorm3(x)
+        x = self.relu(x)
+        x = self.maxpool(x) # 7 x 7 x 128
+        x = self.conv4(x) # 5 x 5 x 256
+        x = self.batchnorm4(x)
+        x = self.relu(x)
+        x = self.conv5(x) # 1 x 1 x 512
+        x = self.relu(x)
+        x = self.batchnorm5(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return x# Create an instance of the model
+
+if __name__ == "__main__":
+    args = config.init_args()
+
+    model = MyModel()
+    optimizer = Adam(learning_rate=args.lr)
+    rmse = RootMeanSquaredError()
+    model.compile(optimizer=optimizer, 
+                  loss='mean_squared_error',
+                  metrics=[rmse])
+    # print(model.summary())
+    catalog_train = load_catalog(args.data_catalog_path)
+    catalog_val = load_catalog(args.val_catalog_path)
+    catalog_test = load_catalog(args.test_catalog_path)
+    sdl_train = SimpleDataLoader(args, catalog_train).prefetch(tf.data.experimental.AUTOTUNE).batch(args.batch_size)
+    sdl_val = SimpleDataLoader(args, catalog_val).prefetch(tf.data.experimental.AUTOTUNE).batch(args.batch_size)
+    model.fit_generator(
+        sdl_train, 
+        steps_per_epoch=None,
+        epochs=5, 
+        validation_data=sdl_val,
+        validation_steps=None
+    )
---
+++ utilities/dataloader_simple.py
@@ -0,0 +1,71 @@
+from utilities.utility import load_catalog
+from utilities.utility import read_ncdf
+from utilities.utility import read_hdf5
+from utilities.utility import map_coord_to_pixel
+from utilities.utility import fetch_channel_samples
+from utilities.utility import plot_and_save_image
+from utilities.config import init_args
+from utilities.config import CROP_SIZE
+# from utilities.utility import get_datetime_attrs
+from matplotlib.patches import Rectangle
+
+import time, threading
+import pdb
+import cv2 as cv
+import os
+import pickle as pkl
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+from tqdm import tqdm
+from utilities.utility import *
+
+avg_x = np.array([0.31950477, 283.18481332, 239.19212155, 272.73521949, 254.09056291]).reshape(1,1,5)
+std_x = np.array([0.27667209, 16.24902932,  8.79865931, 20.08307892, 13.8115307]).reshape(1,1,5)
+
+def load_numpy(filepath):
+    path = os.path.splitext(os.path.basename(filepath))[0] + ".npz"
+    path = os.path.join('npz_store',path)
+    ndarray_dict = np.load(path)
+    return ndarray_dict
+
+class SimpleDataLoader(tf.data.Dataset):
+
+    def __new__(cls, args, catalog):
+
+        return tf.data.Dataset.from_generator(
+            lambda: cls._generator(args,catalog),
+            output_types=(tf.float32,tf.float32)
+            # args=(args,catalog)
+        )
+    def _generator(args, catalog):
+
+        def preprocess(x,y):
+            img = (x - avg_x)/std_x
+            return img,y
+
+        unique_paths = pd.unique(catalog['hdf5_8bit_path'].values.ravel())
+
+        epochs = args.epochs
+        for path in unique_paths:
+            # samples = fetch_all_samples_hdf5(args,path)
+            samples = load_numpy(path)
+
+            grouped = catalog[path == catalog.hdf5_8bit_path]
+            for station in args.station_data.keys():
+                df = grouped[grouped.station == station]
+                argsort = np.argsort(df['hdf5_8bit_offset'].values)
+                offsets_0 = df['hdf5_8bit_offset'].values[argsort]
+
+                GHIs_0 = df[df.hdf5_8bit_offset.isin(offsets_0)].GHI.values
+                CS_GHI_0 = df[df.hdf5_8bit_offset.isin(offsets_0)].CLEARSKY_GHI.values
+                y_0 = CS_GHI_0 - GHIs_0
+
+                sample = samples[station]
+                for i in range(offsets_0.shape[0]):
+                    x = sample[i].swapaxes(0,1).swapaxes(1,2)
+                    y = y_0[i]
+                    x,y = preprocess(x,y)
+                    pdb.set_trace()
+                    yield x,y
